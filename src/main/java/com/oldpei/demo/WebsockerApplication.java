c693507afa13f0ac4b43d4bc25153de6a370be8a
jixing.pei <jixing.pei@sbibits.com>
2020-07-30 14:36:02 +0800

    update

--- src/main/java/com/oldpei/demo/WebsockerApplication.java
+++ src/main/java/com/oldpei/demo/WebsockerApplication.java
@@ -1,13 +1,35 @@
 package com.oldpei.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class WebsockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebsockerApplication.class, args);
+    }
 
+
+    @Value("${server.port}")
+    private String appport;  //站点端口号
+
+    /*当端口启动后，直接跳转界面*/
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        System.out.println("应用已经准备就绪 ... 启动浏览器");
+        String url = "http://localhost:" + appport;
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
